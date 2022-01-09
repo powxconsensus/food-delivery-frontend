d245d7a17cf0eedeabcb3def413e1571cbce105b
jprakash-1 <jainendraprakash13901@gmail.com>
2022-01-09 14:54:49 +0530

    Profile page update

--- src/pages/profile/profile.component.jsx
+++ src/pages/profile/profile.component.jsx
@@ -3,28 +3,30 @@ import { Helmet } from "react-helmet";
 
 const Profile = () => {
     return (
-        <div class="container mt-5">
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-7">
-                    <div class="card p-3 py-4">
-                        <div class="text-center"> <img src="https://i.imgur.com/bDLhJiP.jpg" width="100" class="rounded-circle"/> </div>
-                        <div class="text-center mt-3"> <span class="bg-secondary p-1 px-4 rounded text-white">Food Enthusiast</span>
-                            <h5 class="mt-2 mb-0">John Doe</h5> <span>Foodie</span>
-                            <div class="px-4 mt-1">
-                                <p class="fonts">Loves Biryani </p>
+        <div className="profile">
+        <div class="container mt-5 profilediv">
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-7 ">
+                    <div className="card p-3 py-4">
+                        <div className="text-center profilediv"> <img src="https://bit.ly/3f2vkes" width="100" className="rounded-circle"/> </div>
+                        <div className="text-center mt-3 profilediv"> <span className="bg-secondary p-1 px-4 rounded text-white">Food Enthusiast</span>
+                            <h4 className="mt-2 mb-0">Jainendra Prakash</h4> <span>CEO and Co-Founder</span>
+                            <div className="px-4 mt-1">
+                                <p className="fonts">Butter chicken with naan is my greatest weakness and <br/>my favourite words are focus and now</p>
                             </div>
-                            <ul class="social-list">
-                                <li><i class="fa fa-facebook"></i></li>
-                                <li><i class="fa fa-dribbble"></i></li>
-                                <li><i class="fa fa-instagram"></i></li>
-                                <li><i class="fa fa-linkedin"></i></li>
-                                <li><i class="fa fa-google"></i></li>
+                            <ul className="social-list">
+                                <li><i className="fa fa-facebook"></i></li>
+                                <li><i className="fa fa-dribbble"></i></li>
+                                <li><i className="fa fa-instagram"></i></li>
+                                <li><i className="fa fa-linkedin"></i></li>
+                                <li><i className="fa fa-google"></i></li>
                             </ul>
-                            <div class="buttons"> <button class="btn btn-outline-primary px-4">Message</button> <button class="btn btn-primary px-4 ms-3">Contact</button> </div>
+                            <div className="buttons"> <button className="btn btn-outline-primary px-4">Message</button> <button className="btn btn-primary px-4 ms-3">Contact</button> </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
 
     );
